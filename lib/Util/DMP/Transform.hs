3e46aa03bc0ad2583b39aea29616121929509d7b
Marek Suchánek <marek.suchanek@gmail.com>
2018-06-01 11:31:34 +0200

    Improved exposed interface of dmpTransform

--- lib/Util/DMP/Transform.hs
+++ lib/Util/DMP/Transform.hs
@@ -1,18 +1,28 @@
-module Util.DMP.Transform where
+module Util.DMP.Transform
+       ( dmpTransform
+       , DMPExportType(..)
+       ) where
 
-import Data.Text
+import Data.Text as T
+import Data.Text.Encoding as E
+import Data.ByteString as B
 import Text.Pandoc as Pandoc
 
 import Util.DMP.HTML (dmp2html)
 import Model.FilledKnowledgeModel.FilledKnowledgeModel
 
-dmpTransform :: DMPExportType -> FilledKnowledgeModel -> Maybe Text
-dmpTransform HTML  dmp = Just . pack . dmp2html $ dmp
-dmpTransform eType dmp = case Pandoc.readHtml def (dmp2html dmp) of
+
+-- | Exposed interface for tranforming filled KM to document (as UTF-8 ByteString)
+dmpTransform :: DMPExportType -> FilledKnowledgeModel -> Maybe ByteString
+dmpTransform _ _ = Just . E.encodeUtf8 $ "To be implemented ..."
+
+dmpTransform' :: DMPExportType -> FilledKnowledgeModel -> Maybe Text
+dmpTransform' HTML  dmp = Just . T.pack . dmp2html $ dmp
+dmpTransform' eType dmp = case Pandoc.readHtml def (dmp2html dmp) of
                             Left _ -> Nothing
-                            Right pandoc -> Just . pack . pandocWriter eType def $ pandoc
+                            Right pandoc -> Just . T.pack . pandocWriter eType def $ pandoc
 
-
+-- | Enumeration of supported export document types
 data DMPExportType = HTML
                    | LaTeX
                    | RTF
@@ -20,14 +30,6 @@ data DMPExportType = HTML
                    | Markdown
                    | MediaWiki
                    deriving (Show, Read, Enum, Bounded, Eq)
-
-exportType2String :: DMPExportType -> String
-exportType2String HTML = "html"
-exportType2String LaTeX = "latex"
-exportType2String RTF = "rtf"
-exportType2String RST = "rst"
-exportType2String Markdown = "markdown"
-exportType2String MediaWiki = "mediawiki"
 
 pandocWriter :: DMPExportType -> (Pandoc.WriterOptions -> Pandoc.Pandoc -> String)
 pandocWriter HTML = Pandoc.writeHtmlString
