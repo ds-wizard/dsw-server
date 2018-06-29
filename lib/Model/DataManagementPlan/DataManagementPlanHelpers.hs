0ccca7dcc23b5910013fc9f17b51daaaad6cb58b
Marek Suchánek <marek.suchanek@gmail.com>
2018-06-29 17:33:42 +0200

    DMP format case insensitive matching

--- lib/Model/DataManagementPlan/DataManagementPlanHelpers.hs
+++ lib/Model/DataManagementPlan/DataManagementPlanHelpers.hs
@@ -1,5 +1,7 @@
 module Model.DataManagementPlan.DataManagementPlanHelpers where
 
+import qualified Data.Char as C
+
 import Model.DataManagementPlan.DataManagementPlan
 
 
@@ -18,21 +20,13 @@ formatExtension DokuWiki  = ".dw.txt"
 formatExtension MediaWiki = ".mw.txt"
 formatExtension EPUB2     = ".epub"
 formatExtension EPUB3     = ".epub"
-formatExtension _         = ""
+
+
+allFormats :: [DataManagementPlanFormat]
+allFormats = [minBound..maxBound]
+
+stringFormatMap :: [(String, DataManagementPlanFormat)]
+stringFormatMap = [(map C.toLower . show $ format, format) | format <- allFormats ]
 
 stringToFormat :: String -> Maybe DataManagementPlanFormat
-stringToFormat "json"      = Just JSON
-stringToFormat "html"      = Just HTML
-stringToFormat "latex"     = Just LaTeX
-stringToFormat "html"      = Just Markdown
-stringToFormat "docx"      = Just Docx
-stringToFormat "odt"       = Just ODT
-stringToFormat "pdf"       = Just PDF
-stringToFormat "rtf"       = Just RTF
-stringToFormat "rst"       = Just RST
-stringToFormat "asciidoc"  = Just AsciiDoc
-stringToFormat "dokuwiki"  = Just DokuWiki
-stringToFormat "mediawiki" = Just MediaWiki
-stringToFormat "epub2"     = Just EPUB2
-stringToFormat "epub3"     = Just EPUB3
-stringToFormat _           = Nothing
+stringToFormat str = lookup (map C.toLower str) stringFormatMap
