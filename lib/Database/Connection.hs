94d68508859e9a0e001aef0943b6bdde600a78bf
Marek Suchánek <marek.suchanek@gmail.com>
2018-12-20 17:16:14 +0100

    Verify DB connection when created

--- lib/Database/Connection.hs
+++ lib/Database/Connection.hs
@@ -2,20 +2,26 @@ module Database.Connection where
 
 import Control.Lens ((^.))
 import Data.Text
+import Database.MongoDB (allCollections)
 import Database.Persist.MongoDB
 import Network
 
 import LensesConfig
 
 createDatabaseConnectionPool dswConfig = do
-  createMongoDBPool dbName dbHost dbPort dbCred 1 1 1
+  dbPool <- createMongoDBPool dbName dbHost dbPort dbCred 1 1 1
+  verifyDatabaseConnectionPool dbPool
+  return dbPool
   where
     appConfigDatabase = dswConfig ^. databaseConfig
     dbHost = appConfigDatabase ^. host
     dbPort = PortNumber (fromInteger (appConfigDatabase ^. port) :: PortNumber) :: PortID
     dbName = pack (appConfigDatabase ^. databaseName)
-    dbCred = mkDBCred appConfigDatabase
-    mkDBCred appConfigDatabase =
+    dbCred =
       if appConfigDatabase ^. authEnabled
         then Just $ MongoAuth (pack $ appConfigDatabase ^. username) (pack $ appConfigDatabase ^. password)
         else Nothing
+
+verifyDatabaseConnectionPool dbPool = do
+  _ <- runMongoDBPoolDef allCollections dbPool
+  return ()
