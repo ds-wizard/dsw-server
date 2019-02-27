3c592021a438363d2b4b7f22079e7936a99f9bb6
Marek Suchánek <marek.suchanek@gmail.com>
2019-02-27 19:07:25 +0100

    Working CID and nicer templates

--- lib/Service/Mail/Mailer.hs
+++ lib/Service/Mail/Mailer.hs
@@ -7,6 +7,7 @@ module Service.Mail.Mailer
 import Control.Exception (SomeException, catch, handle)
 import Control.Lens ((^.))
 import Control.Monad.Reader (asks, liftIO)
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as B
 import Data.Either (rights)
@@ -22,6 +23,7 @@ import qualified Network.Mail.Mime as MIME
 import qualified Network.Mail.SMTP as SMTPMail
 import qualified Network.Mime as MIME
 import System.Directory (listDirectory)
+import System.FilePath (takeFileName)
 
 import Constant.Component
 import LensesConfig
@@ -31,47 +33,52 @@ import Model.User.User
 import Util.Logger
 import Util.Template (loadAndRender)
 
-sendRegistrationConfirmationMail :: Email -> U.UUID -> String -> AppContextM ()
-sendRegistrationConfirmationMail email userId hash = do
+sendRegistrationConfirmationMail :: User -> U.UUID -> String -> AppContextM ()
+sendRegistrationConfirmationMail user userId hash = do
   dswConfig <- asks _appContextConfig
   let clientAddress = dswConfig ^. clientConfig . address
       activationLink = clientAddress ++ "/signup-confirmation/" ++ U.toString userId ++ "/" ++ hash
       mailName = dswConfig ^. mail . name
       subject = TL.pack $ mailName ++ ": Confirmation Email"
-      context =
-        fromList
-          [ ("uName", "{{ user.firstName }}")
-          , ("activationLink", activationLink)
-          , ("mailName", mailName)
-          , ("serverURL", clientAddress)
-          ]
-  parts <- loadMailTemplateParts "registrationConfirmation" context
-  sendEmail [email] subject parts
+      additionals = [("activationLink", (Aeson.String $ T.pack activationLink))]
+      context = makeMailContext mailName clientAddress user additionals
+  parts <- loadMailTemplateParts RegistrationConfirmation context
+  sendEmail [user ^. email] subject parts
 
-sendRegistrationCreatedAnalyticsMail :: String -> String -> Email -> AppContextM ()
-sendRegistrationCreatedAnalyticsMail uName uSurname uEmail = do
+sendRegistrationCreatedAnalyticsMail :: User -> AppContextM ()
+sendRegistrationCreatedAnalyticsMail user = do
   dswConfig <- asks _appContextConfig
-  let analyticsAddress = dswConfig ^. analytics . email
+  let clientAddress = dswConfig ^. clientConfig . address
+      analyticsAddress = dswConfig ^. analytics . email
       mailName = dswConfig ^. mail . name
       subject = TL.pack $ mailName ++ ": New user"
-      context = fromList [("uName", uName), ("uSurname", uSurname), ("uEmail", uEmail), ("mailName", mailName)]
-  parts <- loadMailTemplateParts "registrationCreatedAnalytics" context
+      context = makeMailContext mailName clientAddress user []
+  parts <- loadMailTemplateParts RegistrationCreatedAnalytics context
   sendEmail [analyticsAddress] subject parts
 
-sendResetPasswordMail :: Email -> U.UUID -> String -> AppContextM ()
-sendResetPasswordMail email userId hash = do
+sendResetPasswordMail :: User -> U.UUID -> String -> AppContextM ()
+sendResetPasswordMail user userId hash = do
   dswConfig <- asks _appContextConfig
   let clientAddress = dswConfig ^. clientConfig . address
       resetLink = clientAddress ++ "/forgotten-password/" ++ U.toString userId ++ "/" ++ hash
       mailName = dswConfig ^. mail . name
       subject = TL.pack $ mailName ++ ": Reset Password"
-      context = fromList [("resetLink", resetLink), ("mailName", mailName)]
-  parts <- loadMailTemplateParts "resetPassword" context
-  sendEmail [email] subject parts
+      additionals = [("resetLink", (Aeson.String $ T.pack resetLink))]
+      context = makeMailContext mailName clientAddress user additionals
+  parts <- loadMailTemplateParts ResetPassword context
+  sendEmail [user ^. email] subject parts
 
 -- --------------------------------
 -- PRIVATE
 -- --------------------------------
+type MailContext = HashMap T.Text Aeson.Value
+
+data MailType
+  = RegistrationConfirmation
+  | RegistrationCreatedAnalytics
+  | ResetPassword
+  deriving (Show)
+
 makeHTMLPart fn context =
   liftIO $ do
     template <- loadAndRender fn context
@@ -82,16 +89,40 @@ makePlainTextPart fn context =
     template <- loadAndRender fn context
     return $ (SMTPMail.plainTextPart . TL.fromStrict) <$> template
 
-loadMailTemplateParts :: String -> HashMap T.Text String -> AppContextM [MIME.Part]
-loadMailTemplateParts mailName context = do
-  let root = "templates/mail/" ++ mailName
+makeMailContext :: String -> String -> User -> [(T.Text, Aeson.Value)] -> MailContext
+makeMailContext mailName clientAddress user others =
+  fromList $
+  [ ("mailName", Aeson.String $ T.pack mailName)
+  , ("clientAddress", Aeson.String $ T.pack clientAddress)
+  , ("user", Aeson.Object userMap)
+  ] ++
+  others
+  where
+    userMap :: MailContext
+    userMap =
+      fromList
+        [ ("name", Aeson.String $ T.pack $ user ^. name)
+        , ("surname", Aeson.String $ T.pack $ user ^. surname)
+        , ("email", Aeson.String $ T.pack $ user ^. email)
+        , ("role", Aeson.String $ T.pack $ user ^. role)
+        , ("active", Aeson.Bool $ user ^. active)
+        ]
+
+mailToFolderName :: MailType -> String
+mailToFolderName RegistrationConfirmation = "registrationConfirmation"
+mailToFolderName RegistrationCreatedAnalytics = "registrationCreatedAnalytics"
+mailToFolderName ResetPassword = "resetPassword"
+
+loadMailTemplateParts :: MailType -> MailContext -> AppContextM [MIME.Part]
+loadMailTemplateParts mailType context = do
+  let root = "templates/mail/" ++ (mailToFolderName mailType)
   plainTextPart <- makePlainTextPart (root ++ "/message.txt.j2") context
   htmlPart <- makeHTMLPart (root ++ "/message.html.j2") context
   let mainParts = rights [plainTextPart, htmlPart]
   case (htmlPart, plainTextPart) of
-    (Left _, Right _) -> logWarn $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_HTML mailName)
-    (Right _, Left _) -> logWarn $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_PLAIN mailName)
-    (Left _, Left _) -> logError $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_HTML_PLAIN mailName)
+    (Left _, Right _) -> logWarn $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_HTML (show mailType))
+    (Right _, Left _) -> logWarn $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_PLAIN (show mailType))
+    (Left _, Left _) -> logError $ msg _CMP_MAILER (_ERROR_SERVICE_MAIL__MISSING_HTML_PLAIN (show mailType))
     (_, _) -> return ()
   if length mainParts > 0
     then do
@@ -111,8 +142,9 @@ loadFilePart :: String -> IO (Either String MIME.Part)
 loadFilePart filename =
   handle (\(e :: SomeException) -> return . Left . show $ e) $ do
     let contentType = E.decodeUtf8 $ MIME.defaultMimeLookup (T.pack filename)
+        cidHeader = ("Content-ID", T.pack $ "<" ++ takeFileName filename ++ ">")
     filePart <- SMTPMail.filePart contentType filename
-    return $ Right filePart
+    return . Right $ filePart {MIME.partHeaders = (MIME.partHeaders filePart) ++ [cidHeader]}
 
 makeConnection :: Integral i => Bool -> String -> Maybe i -> ((SMTP.SMTPConnection -> IO a) -> IO a)
 makeConnection False host Nothing = SMTP.doSMTP host
