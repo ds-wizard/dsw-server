6d171fc6a00f8823effc62162b6d46d48e8b3b86
Josef Dolezal <pepik.ml@gmail.com>
2019-02-28 21:03:33 +0100

    Create basic implementation for km diffing

--- lib/Service/KnowledgeModelDiff/KnowledgeModelDiff.hs
+++ lib/Service/KnowledgeModelDiff/KnowledgeModelDiff.hs
@@ -1,5 +1,6 @@
 module Service.KnowledgeModelDiff.KnowledgeModelDiff
-( diffKnowledgemodelsById ) where
+  ( diffKnowledgemodelsById
+  ) where
 
 import Model.Error.Error
 import Model.Context.AppContext
@@ -10,22 +11,45 @@ import Service.Package.PackageService
   , heGetAllPreviousEventsSincePackageIdAndUntilPackageId
   )
 import Service.KnowledgeModel.KnowledgeModelApplicator (heCreateKnowledgeModel)
+import Service.Migrator.Applicator.Applicator
 
-data DiffEvent = DiffEvent
-data KnowledgeModelDiff = KnowledgeModelDiff KnowledgeModel [DiffEvent]
+data DiffEvent
+  = NodeAdded String
+  | NodeDeleted String
+  | NodeEdited String
+
+data KnowledgeModelDiff = KnowledgeModelDiff
+  { _knowledgeModelDiffKnowledgeModel :: KnowledgeModel
+  , _knowledgeModelDiffEvents :: [DiffEvent]
+  }
 
 -- Creates new knowledgemodel-like diff tree and diff events based on given knowledge model uuids
 diffKnowledgemodelsById :: String -> String -> AppContextM (Either AppError KnowledgeModelDiff)
-diffKnowledgemodelsById oldKmId newKmId = do
+diffKnowledgemodelsById oldKmId newKmId =
   -- TODO: Validate input data here
   heGetAllPreviousEventsSincePackageId oldKmId $ \oldKmEvents ->
     heCreateKnowledgeModel oldKmEvents $ \oldKm ->
       heGetAllPreviousEventsSincePackageIdAndUntilPackageId newKmId oldKmId $ \newKmEvents ->
-        createKnowledgemodelDiff oldKm newKmEvents
+        case runDiffApplicator oldKm newKmEvents of
+          Left error -> return . Left $ error
+          Right km   -> return . Right $ KnowledgeModelDiff
+            { _knowledgeModelDiffKnowledgeModel = km
+            , _knowledgeModelDiffEvents = createDiffEvents newKmEvents
+            }
 
--- Creates knowledgemodel diff tree by applying events on compiled knowledgemodel.
--- Note: The events must logically follow the knowledgemodel
-createKnowledgemodelDiff :: KnowledgeModel -> [Event] -> AppContextM (Either AppError KnowledgeModelDiff)
-createKnowledgemodelDiff km events = undefined
+-- Runs custom knowledgemodel migration and omits delete events (so deleted paths are still available
+-- for diff events)
+runDiffApplicator :: KnowledgeModel -> [Event] -> Either AppError KnowledgeModel
+runDiffApplicator km events = runApplicator (Just km) editedEvents
+  where editedEvents = filter isNotDeleteEvent events
+        isNotDeleteEvent (DeleteChapterEvent' _)   = False
+        isNotDeleteEvent (DeleteQuestionEvent' _ ) = False
+        isNotDeleteEvent (DeleteAnswerEvent' _)    = False
+        isNotDeleteEvent (DeleteExpertEvent' _)    = False
+        isNotDeleteEvent (DeleteReferenceEvent' _) = False
+        isNotDeleteEvent _                         = True
 
--- Helpers
+createDiffEvents :: [Event] -> [DiffEvent]
+createDiffEvents []     = []
+createDiffEvents (e:es) = [sanitizeEvent e] ++ (createDiffEvents es)
+  where sanitizeEvent _ = NodeEdited ""
